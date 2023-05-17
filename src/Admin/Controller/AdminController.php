4f5718515b6f9010401ca41e4a0b4ca9ca4c5ac7
KoNekoD <moskva111@yahoo.com>
2023-05-17 18:31:37 +0300

    sample controller

--- src/Admin/Controller/AdminController.php
+++ src/Admin/Controller/AdminController.php
@@ -8,13 +8,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin', name: 'app_admin')]
+
 class AdminController extends AbstractController
 {
     public function __construct()
     {
     }
 
+    #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
         return $this->render('admin/index.html.twig');
